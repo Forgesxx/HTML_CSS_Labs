c862ac586f6c5c1e939d0383f2453c68dc42c989
ROG\forge <Forgesx236@gmail.com>
2024-02-20 16:41:42 +0200

    Added a check for an empty string. If you try to enter an empty string, the corresponding message will be displayed.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 const createPost = () => {
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-  
+    const titleInput = document.getElementById("title");
+    const contentInput = document.getElementById("content");
+
+    if (!titleInput.value || !contentInput.value) {
+        alert("Both title and content must be filled out");
+        return; 
+    }
+
+    const title = titleInput.value;
+    const content = contentInput.value;
+
     const postsContainer = document.getElementById("posts-container");
     const newPostContainer = document.createElement("article");
-  
+
     const postTitle = document.createElement("h2");
     const postContent = document.createElement("p");
-  
+
     postTitle.innerText = title;
     postContent.innerText = content;
-  
+
     newPostContainer.appendChild(postTitle);
     newPostContainer.appendChild(postContent);
-  
+
     postsContainer.appendChild(newPostContainer);
-  
-    document.getElementById("title").value = "";
-    document.getElementById("content").value = "";
-  }
+
+    titleInput.value = "";
+    contentInput.value = "";
+}
